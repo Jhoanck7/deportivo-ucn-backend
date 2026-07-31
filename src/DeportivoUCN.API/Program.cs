bd1dbbaaa06dc36fab39a00ca4bb2610933e43c8
Jhoan Montero <jhoan.montero@alumnos.ucn.cl>
2026-07-31 16:40:39 -0400

    ci(cors): allow frontend nip.io domain in backend CORS policy

--- src/DeportivoUCN.API/Program.cs
+++ src/DeportivoUCN.API/Program.cs
@@ -36,7 +36,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:4200", "http://192.168.122.198", "http://localhost") // Permitir desarrollo local y la IP del frontend en la VM
+        policy.WithOrigins("http://localhost:4200", "http://192.168.122.198", "http://192.168.122.198.nip.io", "http://localhost") // Permitir desarrollo, IP y dominio nip.io
               .AllowAnyMethod()
               .AllowAnyHeader();
     });
