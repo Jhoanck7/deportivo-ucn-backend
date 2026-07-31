fc6a9d518f476a9807c25ac1d27ca585a7120c33
Jhoan Montero <jhoan.montero@alumnos.ucn.cl>
2026-07-31 15:48:40 -0400

    ci(cors): allow frontend VM IP in backend CORS policy

--- src/DeportivoUCN.API/Program.cs
+++ src/DeportivoUCN.API/Program.cs
@@ -36,7 +36,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:4200") // Default Angular port
+        policy.WithOrigins("http://localhost:4200", "http://192.168.122.198", "http://localhost") // Permitir desarrollo local y la IP del frontend en la VM
               .AllowAnyMethod()
               .AllowAnyHeader();
     });
